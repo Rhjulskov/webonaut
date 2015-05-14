1ed62bf72e5463e7a481b4b6bee4882ed8d72e55
Rasmus Hjulskov Larsen <rhjulskov@gmail.com>
2015-05-14 15:16:18 +0200

    Added waypoint anymations

--- client/views/pages/frontpage/frontpage.es6.js
+++ client/views/pages/frontpage/frontpage.es6.js
@@ -9,3 +9,27 @@ Template['frontpage'].events({
 });
 
 
+Template.frontpage.rendered = function () {
+  var waypoints = $('.tweet, .instagram').waypoint({
+    handler: function(direction) {
+      if (direction === 'down') {
+        $(this).addClass("visible")
+      } else {
+        $(this).removeClass("visible")
+      }
+    },
+    offset: '80%'
+  })
+
+  var imageAnimation = {width:"50%", ease:Linear.easeNone}
+  var waypoints = $('.imageSection').waypoint({
+    handler: function(direction) {
+      if (direction === 'down') {
+        TweenMax.from(this, 0.5, { marginTop: 200, opacity: 0, immediateRender:true, ease: Back.easeInOut}, 0.1);
+      } else {
+        TweenMax.to(this, 0.5, { marginTop: 200, opacity: 0, immediateRender:true, ease: Back.easeInOut}, 0.1);
+      }
+    },
+    offset: '80%'
+  })
+};
