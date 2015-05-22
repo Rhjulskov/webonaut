e151ba452e69c29393ddcb19ea7ad5060d84eb65
Rasmus Hjulskov Larsen <rhjulskov@gmail.com>
2015-05-22 11:09:43 +0200

    Added moment helper

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -1,3 +1,7 @@
 UI.registerHelper("Posts", function() {
   return Posts.find({}, { sort: { createDate: -1 }});
 });
+
+UI.registerHelper("fromNow", function(date) {
+  return moment(date, "YYYYMMDD").fromNow(); 
+});
