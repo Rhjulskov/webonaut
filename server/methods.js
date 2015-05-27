4aef46571e50d53adf16556d3ecb361d3f21f720
Rasmus Hjulskov Larsen <rhjulskov@gmail.com>
2015-05-27 21:12:44 +0200

    Added debounce function for twitter + instagram api

--- server/methods.js
+++ server/methods.js
@@ -1,23 +1,27 @@
 Meteor.methods({
   addTweet: function(tweet){
-    if(tweets.find({id_str: tweet.id_str}).count() == 0 ){
-      var currentTweet = tweet.text;
+    var currentTweet = tweet.text;
       currentTweet = currentTweet.replace('#webonaut', '');
-      tweets.insert({
-        text: currentTweet,
-        hashtags: tweet.entities.hashtags,
-        id_str: tweet.id_str,
-        published_date: tweet.unixTime,
-        link_href: "https://twitter.com/RasmusHjulskov/status/"+tweet.id_str        
-      });
+
+    var tweetDoc = {
+      text: currentTweet,
+      hashtags: tweet.entities.hashtags,
+      id_str: tweet.id_str,
+      published_date: tweet.unixTime,
+      link_href: "https://twitter.com/RasmusHjulskov/status/"+tweet.id_str        
+    }
+
+    if(tweets.find({id_str: tweet.id_str}).count() == 0 ){
+      tweets.insert(tweetDoc);
+    } else {
+      tweets.update({id_str: tweet.id_str}, {$set: tweetDoc});
     }
   },
   addInsta : function(ig){
-    if(insta.find({id_str: ig.id}).count() == 0){
-      var currentText = ig.caption.text
-      currentText = currentText.replace('#webonaut', '');
-      console.log(ig);
-      insta.insert({
+      var currentText = ig.caption.text,
+        currentText = currentText.replace('#webonaut', '');
+
+      var instaDoc = {
         type: ig.type,
         video: ig.type == "video" ? ig.videos.low_bandwidth.url : null,
         image: ig.images.low_resolution.url,
@@ -26,7 +30,11 @@ Meteor.methods({
         published_date: ig.created_time,
         id_str: ig.id,
         hashtags: ig.tags
-      }) 
+      }
+    if(insta.find({id_str: ig.id}).count() == 0){
+      insta.insert(instaDoc) 
+    } else {
+      insta.update({id_str: ig.id}, {$set: instaDoc});
     }
   }
 });
