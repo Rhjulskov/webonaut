b6ae92b8fc46537031643de61dc41492a955e348
Rasmus Hjulskov Larsen <rhjulskov@gmail.com>
2015-05-30 21:34:02 +0200

    Social grid adjustment on smaller screens

--- client/views/atoms/gridEnd/gridEnd.es6.js
+++ client/views/atoms/gridEnd/gridEnd.es6.js
@@ -1,16 +1,17 @@
 Template.gridEnd.rendered = function () {
   // setTimeout(function(){
-    console.log("frontpage rendered Working");
-    var waypoints = $('.tweet, .instagram, .imageSection').waypoint({
-      handler: function(direction) {
-        console.log("adding")
-        if (direction === 'down') {
-          $(this).addClass("visible")
-        } else {
-          $(this).removeClass("visible")
-        }
-      },
-      offset: '80%'
-    })
+    var initialize = _.once(function(){
+      var waypoints = $('.tweet, .instagram, .imageSection').waypoint({
+        handler: function(direction) {
+          if (direction === 'down') {
+            $(this).addClass("visible")
+          } else {
+            $(this).removeClass("visible")
+          }
+        },
+        offset: '80%'
+      })
+    });
+    initialize()
   // },23000)
 }
