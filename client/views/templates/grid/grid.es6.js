0f14778c1d4cec935b124edf34ceaa3e1da59bbc
Rasmus Hjulskov Larsen <rhjulskov@gmail.com>
2015-06-17 19:33:12 +0200

    Cleaned up grid + footer views

--- client/views/templates/grid/grid.es6.js
+++ client/views/templates/grid/grid.es6.js
@@ -1,16 +1,5 @@
 // Grid callback loaded in gridEnd template
 Template['grid'].helpers({
-  codepens: () => [
-    {
-      "src": "//codepen.io/Rhjulskov/embed/ravzGw/?height=400&theme-id=13167"
-    }, 
-    {
-      "src": "//codepen.io/bugarin/embed/BNjZZP/?height=400&theme-id=13100&default-tab=result"
-    },
-    {
-      "src": "//codepen.io/Yakudoo/embed/eNmjEv/?height=400&theme-id=13100&default-tab=result"
-    }
-  ],
   socialFeeds: function(){
     var tweetsFeed = tweets.find().fetch();
     var instaFeed = insta.find().fetch();
