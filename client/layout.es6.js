8aa9fef93e9d7454553ae62d21daf793d237d5fc
Rasmus Hjulskov Larsen <rhjulskov@gmail.com>
2015-07-11 18:14:35 +0200

    syntax typo

--- client/layout.es6.js
+++ client/layout.es6.js
@@ -4,7 +4,7 @@ Meteor.startup(function(){
   return SEO.config({
     title: "Webonaut | Personal portfolio of a fullstack Meteor.js developer",
     meta: {
-      'description': "Fullstack delevoper based in Aarhus, Denmark focusing on web apps development in Meteor.js.  I'm blogging and organizing a meetup group about Meteor.js.""
+      'description': "Fullstack delevoper based in Aarhus, Denmark focusing on web apps development in Meteor.js.  I'm blogging and organizing a meetup group about Meteor.js."
     },
     og: {
       'image': '/webonaut_profile_blue.png'
