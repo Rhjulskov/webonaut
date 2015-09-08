e55713dc9ddfe8da9beba9609ac2b8b905c5d324
Rasmus Hjulskov <rhjulskob@gmail.com>
2015-09-08 18:57:05 +0200

    Add medium to grid layout

--- client/views/templates/grid/grid.es6.js
+++ client/views/templates/grid/grid.es6.js
@@ -1,18 +1,23 @@
 // Grid callback loaded in gridEnd template
 Template['grid'].helpers({
   socialFeeds: function(){
-    var codepenFeed =  [
+    var staticLinks =  [
         {
             "type": "codepen",
             "src":  "//codepen.io/Rhjulskov/embed/vOMoaG/",
             "published_date": 1439596800
+        },
+        {
+          "type": "medium",
+          "src": "//medium.com/twenty-years-in-the-valley/one-of-the-biggest-mistakes-i-ve-made-in-my-career-72bf27c538b4",
+          "published_date": 1439596801
         }
     ]
 
     var tweetsFeed = tweets.find().fetch();
     var instaFeed = insta.find().fetch();
 
-    var docs = tweetsFeed.concat(instaFeed).concat(codepenFeed);
+    var docs = tweetsFeed.concat(instaFeed).concat(staticLinks);
     var array = _.sortBy(docs, function(doc) {return doc.published_date});
     array.unshift({"grid__end":true});
     return array.reverse();
@@ -21,6 +26,3 @@ Template['grid'].helpers({
 
 Template['grid'].events({
 });
-
-
-
